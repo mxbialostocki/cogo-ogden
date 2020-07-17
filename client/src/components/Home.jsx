976ef6731d8545423ba20dff683df214404779bb
mxbialostocki <matt.bialostocki@gmail.com>
2020-07-17 17:09:27 +1200

    add some comments for cleaner code

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -10,16 +10,18 @@ const Home = ({ogden}) => {
   // create empty array to store rejected words
   const [ rejectedWords, setRejectedWords ] = useState([])
 
+  // deconstruct the props
   const ogdenString = ogden
-
+  // turn the string into an array for clean validation
   const validWordsArray = ogdenString.split(' ')
 
-  
+  // check woirds in a string against Ogden
   function checkWords(string) {
     // create an array from the input
     const inputArray = string.split(' ')
 
     const errorMessage = "oh no! the following words are invalid:"
+
     inputArray.map(word => {
       if (validWordsArray.includes(word)) {
         return
@@ -27,6 +29,7 @@ const Home = ({ogden}) => {
         rejectedWords.push(word)
       }
     })
+    // if any invalid words were found, notify user
     if (rejectedWords.length >= 1) {
       setEvaluation(errorMessage)
     } else {
@@ -40,13 +43,15 @@ const Home = ({ogden}) => {
     <React.Fragment>
       <Grid container direction="column" style={{ width: '80%' }}>
         <Grid item style={{ width: '80%' }}>
-          <Input disableUnderline style={{ width: '80%' }}size="big" type="text" value={inputString} onChange={(event) => { setInputString(event.target.value) }} placeholder="Please type some words here!"/>
+          <Input disableUnderline style={{ width: '80%' }} size="big" type="text" value={inputString} onChange={(event) => { setInputString(event.target.value) }} placeholder="Please type some words here!"/>
         </Grid>
         <Grid item>
           <Button size={"large"} onClick={() => {
+            // if the user has typed something in, check
             if (inputString) {
               checkWords(inputString)
             }
+            // reset input upon submission
             setInputString('')
           }}>
             Check Spelling
